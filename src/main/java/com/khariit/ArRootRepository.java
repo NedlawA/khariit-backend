4b27dc83d10b8b7d7c5d33485ecd19bf7a096936
Arianna Walden <nedlawa@gmail.com>
2023-07-27 11:08:11 -0700

    remove unecesary nesting

--- src/main/java/com/khariit/ArRootRepository.java
+++ src/main/java/com/khariit/ArRootRepository.java
@@ -1,8 +1,7 @@
 package com.khariit;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArRootRepository
-    extends JpaRepository<Root, Integer>{
+    extends JpaRepository<Root, Integer> {
 }
