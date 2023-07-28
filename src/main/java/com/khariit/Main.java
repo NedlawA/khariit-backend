fecc685fa136a8f8b24871995737d2c53c2463f3
Arianna Walden <nedlawa@gmail.com>
2023-07-28 09:13:59 -0700

    add CORS and change port

--- src/main/java/com/khariit/Main.java
+++ src/main/java/com/khariit/Main.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 @SpringBootApplication
 @RestController
 @RequestMapping("api/v_01/words")
+@CrossOrigin(origins = "http://localhost:3000")
 public class Main {
     private final ArRootRepository arRootRepository;
 
